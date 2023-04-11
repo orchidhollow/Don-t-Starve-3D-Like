7fb80703bc34bba10c0bb9339732bbfa41072c43
orchidhollow <895002501@qq.com>
2023-04-11 22:20:18 +0800

    2023/4/11

--- Assets/Script/Pig_Controller.cs
+++ Assets/Script/Pig_Controller.cs
@@ -31,8 +31,10 @@ public class Pig_Controller : ObjectBase
 
     private EnemyState enemyState;
 
+    private Vector3 targetPos;
+
     //当状态修改时，进入进状态要做的事情
-    public EnemyState EnemyState
+    public EnemyState EnemyState//调用,改变时触发
     {
         get => enemyState;
 
@@ -42,21 +44,149 @@ public class Pig_Controller : ObjectBase
             switch (enemyState)
             {
                 case EnemyState.Idle:
+                    //播放动画
+                    //关闭导航
+                    //休息一段时间之后去巡逻
+                    Animator.CrossFadeInFixedTime("Idle", 0.25f);//过渡播放动画：动画名称，过渡时间
+                    navMeshAgent.enabled= false;
+                    Invoke(nameof(GoMove), Random.Range(3f, 10f));//过一段时间调用：方法，时间
                     break;
                 case EnemyState.Move:
+                    //播放动画
+                    //开启导航
+                    //获取巡逻点
+                    //移动到目标位置
+                    Animator.CrossFadeInFixedTime("Move", 0.25f);
+                    navMeshAgent.enabled = true;
+                    targetPos = GetTargetPos();
+                    navMeshAgent.SetDestination(targetPos);
                     break;
                 case EnemyState.Pursue:
+                    Animator.CrossFadeInFixedTime("Move", 0.25f);
+                    navMeshAgent.enabled = true;
                     break;
                 case EnemyState.Attack:
+                    Animator.CrossFadeInFixedTime("Attack", 0.25f);
+                    transform.LookAt(PlayerController.instance.transform.position);
+                    navMeshAgent.enabled = false;
                     break;
                 case EnemyState.Hurt:
+                    Animator.CrossFadeInFixedTime("Hurt", 0.25f);
+                    PlayAudio(0);
+                    navMeshAgent.enabled = false;
                     break;
                 case EnemyState.Die:
-                    break;
-                default:
+                    Animator.CrossFadeInFixedTime("Die", 0.25f);
+                    PlayAudio(0);
+                    navMeshAgent.enabled = false;
                     break;
             }
         }
-        
     }
+
+    private void Start()
+    {
+        EnemyState =EnemyState.Idle;
+        checkCollider.Init(this, 10);
+    }
+    private void Update()
+    {
+        StateOnUpdate();
+    }
+
+    private void StateOnUpdate()//一直检测，一直运行
+    {
+        switch (enemyState)
+        {
+            //非一直检测的可以删除
+            //case EnemyState.Idle:
+            //    break;
+            case EnemyState.Move:
+                if(Vector3.Distance(transform.position,targetPos)<1.5f)
+                {
+                    EnemyState= EnemyState.Idle;
+                }
+                break;
+            case EnemyState.Pursue:
+                //距离足够近，攻击
+                if (Vector3.Distance(transform.position,PlayerController.instance.transform.position)<1)
+                {
+                    EnemyState = EnemyState.Attack;
+                }
+                else
+                {
+                    //距离遥远，继续追
+                    navMeshAgent.SetDestination(PlayerController.instance.transform.position);
+                }
+                break;
+            //case EnemyState.Attack:
+            //    break;
+            //case EnemyState.Hurt:
+            //    break;
+            //case EnemyState.Die:
+            //    break;
+        }
+    }
+
+    private void GoMove()
+    {
+        EnemyState = EnemyState.Move;
+    }
+
+    //获得一个范围内随机点
+    private Vector3 GetTargetPos()
+    {
+        return new Vector3 (Random.Range(minX ,maxX),0,Random.Range(minZ,maxZ));
+    }
+
+    public override void Hurt(int damage)
+    {
+        if (EnemyState==EnemyState.Die) return;
+        CancelInvoke(nameof(GoMove));//取消切换到移动状态的延迟调用
+        base.Hurt(damage);
+        if (Hp > 0)
+        {
+            EnemyState = EnemyState.Hurt;
+        }
+    }
+
+    protected override void Dead()
+    {
+        base.Dead();
+        EnemyState = EnemyState.Die;
+    }
+
+    #region 动画事件
+    private void StartHit()
+    {
+        checkCollider.StarHit();
+    }
+
+    private void StopHit()
+    {
+        checkCollider.StopHit();
+    }
+
+    private void StopAttack()
+    {
+        if (EnemyState != EnemyState.Die)
+        {
+            EnemyState = EnemyState.Pursue;
+        }
+    }
+
+    private void HurtOver()
+    {
+        if (EnemyState!=EnemyState.Die)
+        {
+            EnemyState = EnemyState.Pursue;
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    #endregion
 }
